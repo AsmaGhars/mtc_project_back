efc97196d68bdc2b19f9e8ac1db26f4aedd3e5a3
unknown <gharsallahasma555@gmail.com>
2023-02-07 20:11:58 +0100

    asma

---
+++ server.js
@@ -0,0 +1,8 @@
+// import app from backend/app.js
+const app = require("./backend/app");
+
+
+// app is listening on port 3000
+app.listen(3000,() =>{
+    console.log("APP is listening on port 3000 ... ");
+});
